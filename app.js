20aec55e6530fee6f2d8c96f773bbf49653440df
anna mendoza <anna@curvecommunications.biz>
2016-07-24 12:31:08 -0700

    foundation for the project

---
+++ app.js
@@ -0,0 +1,20 @@
+var fizzBuzz = function() {
+	var num = 100;
+	var result;
+	for (var i = 1; i <= num; i++) {
+		if ( i % 3 === 0 && i % 5 === 0 ) {
+			console.log("FizzBuzz");
+		} else if ( i % 3 === 0 ) {
+			console.log("Fizz");
+		} else if ( i % 5 === 0 && i % 5 === 0 ) {
+			console.log("Buzz");
+	 	} else {
+	 		result = i
+	 		console.log(result);
+	 	}
+	 }
+};
+	 fizzBuzz();
+
+
+
